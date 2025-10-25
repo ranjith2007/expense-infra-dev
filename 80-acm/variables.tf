ebf216c3e12d3ee396a0d7ff6c929a3276daac72
ranjith2007 <ranjithkumar0799@gmail.com>
2025-10-25 23:14:18 +0530

    terraform

---
+++ 80-acm/variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+  default = "expense"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "comman_tags" {
+  default = {
+    Project     = "expense"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+variable "zone_name" {
+  default = "devops81s.site"
+}
+
+variable "zone_id" {
+  default = "Z0673714ZP6J86LIKLFQ"
+}
